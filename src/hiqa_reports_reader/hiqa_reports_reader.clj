82dbb78f2535693f4920dda0a753482ecc01a470
Eoin Carney <eoincarney0@gmail.com>
2024-01-28 13:00:05 +0000

    changed mount fn

--- src/hiqa_reports_reader/hiqa_reports_reader.clj
+++ src/hiqa_reports_reader/hiqa_reports_reader.clj
@@ -1,14 +1,8 @@
 (ns hiqa-reports-reader.hiqa-reports-reader
   (:gen-class)
-  (:require [hiqa-reports-reader.reader.main :refer [process-and-write-pdfs!]]))
-
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+  (:require [hiqa-reports-reader.ui.main :as ui]
+            [cljfx.api :as fx]))
 
 (defn -main
-  [pdf-directory]
-  (greet {:name "Test"})
-  (process-and-write-pdfs! pdf-directory)
-  (shutdown-agents))
+  []
+  (fx/mount-renderer ui/*state ui/renderer))
