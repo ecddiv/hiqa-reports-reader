1b5cfc5645f7d7ab87af4bb64d19ac568b289d82
Eoin Carney <eoincarney0@gmail.com>
2024-03-01 13:24:57 +0000

    updated ui components and added future thread

--- src/hiqa_reports_reader/ui/main.clj
+++ src/hiqa_reports_reader/ui/main.clj
@@ -9,9 +9,36 @@
    (javafx.scene Node)
    (javafx.stage DirectoryChooser)))
 
+
+
 (def *state
   (atom {:file nil
-         :logs nil}))
+         :pdf-count nil
+         :logs nil
+         :runningp false}))
+
+(defn count-pdfs [file]
+  (->> file
+       io/file
+       file-seq
+       rest
+       (filter #(re-find #".pdf" (str %)))
+       count))
+
+(defn seconds->minutes [seconds]
+  (if (< seconds 60)
+    (str (int seconds) " seconds")
+    (let [mins (int (Math/floor (/ seconds 60)))
+          secs (int (mod seconds 60))]
+      (str mins (if (= mins 1) " minute and " "minutes and ") secs " seconds"))))
+
+;; Avg rate of running - 22s per 1000 pdfs
+(defn estimate-completion-time []
+  (let [pdf-count (:pdf-count @*state)]
+    (if pdf-count
+      (seconds->minutes
+       (* 2.2 (/ pdf-count 100)))
+      "0")))
 
 (defmulti handle ::event)
 
@@ -19,16 +46,25 @@
   (let [window (.getWindow (.getScene ^Node (.getTarget event)))
         chooser (doto (DirectoryChooser.)
                   (.setTitle "Select folder containing report pdfs"))]
-    (when-let [file (.showDialog chooser window)]
-      {:state {:file file}})))
+    (when (not (:runningp *state))
+      (when-let [file (.showDialog chooser window)]
+        {:state {:file file
+                 :pdf-count (count-pdfs file)}}))))
 
-(defn count-files [file]
-  (->> file
-       io/file
-       file-seq
-       rest
-       (filter #(re-find #".pdf" (str %)))
-       count))
+
+(defn process-pdfs [_]
+  (when (and (not (:runningp @*state))
+             (:file @*state))
+    (do (swap! *state assoc :logs (str "Running... \nEstimated completion time: " (estimate-completion-time)))
+        (future
+          (try
+            (let [{:keys [table table-recents info]} (reader/make-pdf-table (:file @*state))
+                  {:keys [row-count table-info]} info]
+              (do (reader/process-and-write-outputs! table table-recents table-info)
+                  (swap! *state assoc :logs (str "Done. " row-count " files processed."))))
+            (catch Exception e
+              (.printStackTrace e)
+              (swap! *state assoc :logs "Failed.")))))))
 
 (defn root-view [{:keys [file]}]
   {:fx/type :stage
@@ -44,19 +80,16 @@
                               :spacing 15
                               :alignment :center-left
                               :children [{:fx/type :button
-                                          :text "Open file..."
+                                          :text "Select Folder..."
                                           :on-action {::event ::select-directory}}
                                          {:fx/type :label
                                           :text (str file)}
                                          {:fx/type :label
-                                          :text (when file (str "Folder contains " (count-files file) " pdfs."))}]}
+                                          :text (when file
+                                                  (str "Folder contains " (:pdf-count @*state) " pdfs."))}]}
                              {:fx/type :button
                               :text "Run"
-                              :on-action (fn [_]
-                                           (let [{:keys [table table-recents info]} (reader/make-pdf-table (:file @*state))
-                                                 {:keys [row-count table-info]} info]
-                                             (do (reader/process-and-write-outputs! table table-recents table-info)
-                                                 (swap! *state assoc :logs (str "Done. " row-count " files processed.")))))}
+                              :on-action process-pdfs}
                              {:fx/type :label
                               :text (str (:logs @*state))}]}}})
 
